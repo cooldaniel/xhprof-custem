229ab7c6cc39f1f72c4f5fb9893bbd7b6e7af4ec
cooldaniel <295313207@qq.com>
2021-12-17 15:13:07 +0800

    xhprof_site_list

--- auto_prepend_xhprof.php
+++ auto_prepend_xhprof.php
@@ -6,14 +6,38 @@ xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
 //在程序结束后收集数据
 register_shutdown_function(function() {
 
+    // 只有允许记录的站点才会记录
+    // 以站点目录的方式指定
+    if (empty($GLOBALS['xhprof_site_list'])) {
+        return;
+    }
+
+    $allow_log = false;
+    foreach ($GLOBALS['xhprof_site_list'] as $item) {
+        if (!empty($_SERVER['DOCUMENT_ROOT'])) {
+            if (strpos($_SERVER['DOCUMENT_ROOT'], $item)) {
+                $allow_log = true;
+                break;
+            }
+        } else {
+            if (strpos($_SERVER['PWD'], $item)) {
+                $allow_log = true;
+            }
+        }
+    }
+
+    if (!$allow_log) {
+        return;
+    }
+
     $xhprof_data = xhprof_disable();
 
 //    \D::logc(array_sum(array_column($xhprof_data, 'ct')), $xhprof_data);
 //    \D::log($xhprof_data["main()"]);
 //    \D::log($xhprof_data["main()"]['wt']);
-    if ($xhprof_data["main()"]['wt'] < 2*1000*1000) {
-        return;
-    }
+//    if ($xhprof_data["main()"]['wt'] < 2*1000*1000) {
+//        return;
+//    }
 
 //    if (!$xhprof_data["main()"]['mu'] && !$xhprof_data["main()"]['pmu']) {
 //        return;
@@ -34,7 +58,10 @@ register_shutdown_function(function() {
         $url = str_replace('/', '|', $url);
         $url = str_replace('.', '_', $url);
 
-        $run_id = substr($url, 0, 150) . '-' .  uniqid();
+        $num = $xhprof_data["main()"]['wt'];
+        $num = $num / 1000;
+        $num = $num / 1000;
+        $run_id = substr($url, 0, 150) . '-' .  uniqid() . '-----' . $num . '-----';
         
         //file_put_contents('/var/xhprof/dd.txt', $run_id."\n", FILE_APPEND);
     }else{
